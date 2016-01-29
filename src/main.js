2a382b1e98e145b457f52fb25689860b7ee522e0
Jim Sagevid <sotaio@gmail.com>
2016-01-29 17:09:02 +0100

    Added an icon.

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ app.on('window-all-closed', () => app.quit());
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
+        icon: path.resolve(path.join(__dirname, "..", "resources", "stingray.ico")),
         width: 1440,
         height: 900,
         preload: path.join(__dirname, "app.js")
